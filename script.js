29553cb034e1ece7891240a88b9f57c937e6418d
nguyd1 <davennguy@gmail.com>
2021-11-02 15:28:04 -0700

    Add h1 winner

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-// players use factory
-// one of something (gameBoard displayerController) use modules
+// multiples of something (players) use factories
+// one of something (gameBoard displayController) use modules
+
+// players stored in object
+const playerFactory=(name)=>{
+    const sayHello=()=>console.log("hello");
+    return {name,sayHello};
+};
+
+// allow players to put names
+const name1=prompt("Enter name of Player 1:");
+const name2=prompt("Enter name of Player 2:");
+
+const player1=playerFactory(name1);
+const player2=playerFactory(name2);
 
 // start with storing gameboard array in gameboard object
-// players stored in object
+const gameBoard=(()=>{
+    let board=[];
+})();
+
+
 // flow of game stored in object
+const displayController=(()=>{
+
+})();
 
 // build function to allow players to mark spots with dom
 
+
 // check for game over each move
 
-// allow player to put name, start/restart button
+
+// start/restart button
+const restart=document.querySelector(".restart");
+restart.addEventListener("click",()=>{
+
+});
+
 // display winner
