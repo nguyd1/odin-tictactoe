df387527a0524c08ce9e662c84040d41aed83ca6
nguyd1 <davennguy@gmail.com>
2021-11-02 14:51:26 -0700

    Implement grid

---
+++ script.js
@@ -0,0 +1,13 @@
+// players use factory
+// one of something (gameBoard displayerController) use modules
+
+// start with storing gameboard array in gameboard object
+// players stored in object
+// flow of game stored in object
+
+// build function to allow players to mark spots with dom
+
+// check for game over each move
+
+// allow player to put name, start/restart button
+// display winner
